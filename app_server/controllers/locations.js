cae736bf5b6e04fa8c6096121c225885837d0424
rakeshthakkuri <rakeshthakkuri969@gmail.com>
2024-08-29 15:43:23 +0530

    first commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,31 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+    res.render('locations-list', {
+    title: 'CAFÈ ',
+    pageHeader: {
+    title: 'CAFÈ Shop',
+    //strapline: 'Find places to work with wifi near you!'
+    },
+    locations: [{
+    name: 'Mug Stop Cafè',
+    address: 'Plot 42-44, Serial 8, 19-22, Gachibowli, Hyderabad',
+    rating: 4,
+    facilities: ['Continental', ' Italian', 'Sandwich', 'Pizza', 'Beverages'],
+    distance: '2.6KM From Gachibowli'
+     }
+]
+});
+};
+   /* GET 'Location info' page */
+   const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location info' });
+   };
+   /* GET 'Add review' page */
+   const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });
+   };
+   module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+   };
